da5a0089d837a3297ab38b3dbf3c81025b56f686
jonathan <tintinpqkg@gmail.com>
2025-05-04 14:51:36 +1000

    done

---
+++ DMS3/experiment/A2/cursor.js
@@ -0,0 +1,16 @@
+const cursor = document.querySelector(".custom-cursor");
+
+document.addEventListener("mousemove", (e) => {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
+
+const h1 = document.querySelector("h1");
+
+h1.addEventListener("mouseenter", () => {
+  cursor.classList.add("cursor-hover");
+});
+
+h1.addEventListener("mouseleave", () => {
+  cursor.classList.remove("cursor-hover");
+});
